c77893266ebf37d22d68fb7ef1206be9619c17a2
Salim DELLALI <dellali.salim@gmail.com>
2022-03-06 14:10:05 +0100

    first commit, set up basic Rest Api

---
+++ server.js
@@ -0,0 +1,25 @@
+// import npm packages
+const express = require('express');
+const cors = require('cors');
+
+// Init app
+const app = express();
+
+// to allow the API to be fetched from anywhere
+app.use(cors());
+
+// to be able to parse json data and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// test if / route works
+app.get('/', (req, res) => {
+	res.send('we are on root');
+});
+
+// set PORT to listen to
+const PORT = process.env.PORT || 1234;
+app.listen(PORT, (err) => {
+	if (err) console.error(err);
+	console.log(`Server started on port ${PORT} ...`);
+});
